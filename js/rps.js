43023feba8020ec40d2f97e02198236e26664168
siobyrne <siobyrne@users.noreply.github.com>
2023-04-01 17:13:02 -0400

    Add function that randomly chooses a value for the computer

--- js/rps.js
+++ js/rps.js
@@ -1,1 +1,23 @@
 // Rock, Paper, Scissors Game
+
+
+function getComputerChoice(array) {
+
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+        // swap elements array [i] and array[j]
+        [array[i], array[j]] = [array[j], array[i]];
+
+        }
+        
+        console.log(array[0]);  
+        return array[0];
+}
+
+let choices = ['rock', 'paper', 'scissors'];
+
+getComputerChoice(choices);
+
+
+
